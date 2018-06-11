54637b5d06a8a531a54069ba4468de579e900ada
Mikhail <miha.vanya@gmail.com>
2018-06-11 22:30:40 +0500

    Add project files.

---
+++ WebLibraryProject2/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebLibraryProject2.Startup))]
+namespace WebLibraryProject2
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
